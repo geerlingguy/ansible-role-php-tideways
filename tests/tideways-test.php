b88fab25128d3c65e445455fc11b02e5fd55ba47
Jeff Geerling <geerlingguy@mac.com>
2017-01-13 23:19:08 -0600

    Get initial Tideways test working.

--- tests/tideways-test.php
+++ tests/tideways-test.php
@@ -8,44 +8,19 @@
 $tideways_root_dir = '/usr/share/php';
 $success = TRUE;
 
-// TODO - Convert from XHProf...
-xhprof_enable();
-$data = xhprof_disable();
+tideways_enable();
+$data = tideways_disable();
 
-if (isset($data['main()==>xhprof_disable'])) {
-  print "XHProf profiling working.\r\n";
+if (isset($data['main()==>tideways_disable'])) {
+  print "Tideways profiling working.\r\n";
 }
 else {
-  print "XHProf profiling not working.\r\n";
+  print "Tideways profiling not working.\r\n";
   $success = FALSE;
 }
 
 include $xhprof_root_dir . '/xhprof_lib/utils/xhprof_lib.php';
 include $xhprof_root_dir . '/xhprof_lib/utils/xhprof_runs.php';
-
-$xhprof_runs = new XHProfRuns_Default();
-$run_id = $xhprof_runs->save_run($data, "xhprof_testing");
-$output_dir = ini_get("xhprof.output_dir");
-$filename = "$output_dir/$run_id.xhprof_testing.xhprof";
-
-if (file_exists($filename)) {
-  print "XHProf PHP library writing to output directory.\r\n";
-}
-else {
-  print "XHProf PHP library not working.\r\n";
-  $success = FALSE;
-}
-
-ob_start();
-include $xhprof_root_dir . '/xhprof_html/index.php';
-$html = ob_get_clean();
-if (strpos($html, "?run=$run_id") !== FALSE) {
-  print "XHProf UI working.\r\n";
-}
-else {
-  print "XHProf UI not working.\r\n";
-  $success = FALSE;
-}
 
 if (!$success) {
   exit(1);
